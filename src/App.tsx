8bbce1c2002e0e02e10fb8afb97cfe098bf9352c
Ayoe <anomalous189@gmail.com>
2022-07-03 00:01:25 +0100

    first build

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   // console.log(coins)
   
   
-  const url: string = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d'
+  const url: string = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d'
 
 
   //fetch coin data
